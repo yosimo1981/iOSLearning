3fb8741864de23e1e0d6dd4cee9154d4fb341289
yosimo1981 <yosimo1981@yahoo.co.jp>
2019-07-12 22:00:26 +0900

    プロジェクト修正の戻し

--- FaceTracker/ViewController.swift
+++ FaceTracker/ViewController.swift
@@ -9,26 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
     var faceTracker:FaceTracker? = nil;
     
-
     @IBOutlet var cameraView: UIView!//viewController上に一つviewを敷いてそれと繋いでおく
     
-    var rectView = UIView()
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        self.rectView.layer.borderWidth = 3//四角い枠を用意しておく
-        self.view.addSubview(self.rectView)
-        faceTracker = FaceTracker(view: self.cameraView, findface:{arr in
+        
+        let rectLayer = CALayer()
+        rectLayer.borderWidth = 3//四角い枠を用意しておく
+        
+        self.cameraView.layer.addSublayer(rectLayer)
+        faceTracker = FaceTracker(view: self.cameraView, layer: rectLayer, findface:{arr in
             let rect = arr[0];//一番の顔だけ使う
-            self.rectView.frame = rect;//四角い枠を顔の位置に移動する
+            rectLayer.frame = rect;//四角い枠を顔の位置に移動する
         })
     }
 }
-
